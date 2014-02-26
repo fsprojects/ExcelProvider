83dcdd1097f0b6838b68700cbd21ca0b662bfdd9
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-02-26 06:07:41 -0800

    Initial commit

---
+++ RELEASE_NOTES.md
@@ -0,0 +1,2 @@
+#### 0.0.1-alpha - 21.02.2014
+* Initial release of ExcelProvider
---
+++ src/ExcelProvider/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("ExcelProvider")>]
+[<assembly: AssemblyProductAttribute("ExcelProvider")>]
+[<assembly: AssemblyDescriptionAttribute("A type provider for regular expressions.")>]
+[<assembly: AssemblyVersionAttribute("0.0.1")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.1")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "0.0.1"
