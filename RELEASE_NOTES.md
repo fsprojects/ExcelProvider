44c1a949b2d9625a2a527830443500984c167635
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-03-20 08:10:57 +0100

    New version

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,2 +1,8 @@
+#### 0.1.0 - 20.03.2014
+* Upgrading to the latest version of the ExcelDataReader library.
+* Handling loading higher version of ICSharpCode.SharpZipLib.
+* Defaulting to the first sheet in the spreadsheet if a sheet is not specifically indicated.
+* Using a Row type, with a ToString() method
+
 #### 0.0.1-alpha - 21.02.2014
 * Initial release of ExcelProvider
--- src/ExcelProvider/AssemblyInfo.fs
+++ src/ExcelProvider/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("ExcelProvider")>]
 [<assembly: AssemblyProductAttribute("ExcelProvider")>]
 [<assembly: AssemblyDescriptionAttribute("This library is for the .NET platform implementing a Excel type provider.")>]
-[<assembly: AssemblyVersionAttribute("0.0.1")>]
-[<assembly: AssemblyFileVersionAttribute("0.0.1")>]
+[<assembly: AssemblyVersionAttribute("0.1.0")>]
+[<assembly: AssemblyFileVersionAttribute("0.1.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.0.1"
+    let [<Literal>] Version = "0.1.0"
