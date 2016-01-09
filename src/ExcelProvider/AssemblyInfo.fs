dab0ec9f45436fa5134183a2a34d997803e17661
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2016-01-09 14:27:04 +0100

    Bump version to 0.2.4

--- src/ExcelProvider/AssemblyInfo.fs
+++ src/ExcelProvider/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("ExcelProvider")>]
 [<assembly: AssemblyProductAttribute("ExcelProvider")>]
 [<assembly: AssemblyDescriptionAttribute("This library is for the .NET platform implementing a Excel type provider.")>]
-[<assembly: AssemblyVersionAttribute("0.2.3")>]
-[<assembly: AssemblyFileVersionAttribute("0.2.3")>]
+[<assembly: AssemblyVersionAttribute("0.2.4")>]
+[<assembly: AssemblyFileVersionAttribute("0.2.4")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.2.3"
+    let [<Literal>] Version = "0.2.4"
