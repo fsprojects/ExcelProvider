6b0de92da90df507eb27235e4e8f05b24fa7ef06
pbbwfc <phil.brooks@asr-services.com>
2016-01-16 17:35:49 +0000

    Extended docs
    *Created Documentation section with subsections covering different
    aspects
    *Simplified index page and referenced new sections
    *Needed to update libraries as bug in Fake prevent correct build
    *Changed generate.fsx to allow embedding output values.

---
+++ docs/content/rows.fsx
@@ -0,0 +1,31 @@
+(*** hide ***)
+#I "../../bin/ExcelProvider"
+
+(**
+Accessing Rows
+===========================
+
+Rows are returned as a sequence from the `Data` element of the ExcelFile type.
+
+Y 
+
+Example
+-------
+
+![alt text](img/MultSheets.png "Excel sample file with multiple sheets")
+
+This example demonstrates loadings the second row (with index 1) into the variable test:
+
+*)
+
+// reference the type provider dll
+#r "ExcelProvider.dll"
+open FSharp.ExcelProvider
+
+// Let the type provider do it's work
+type MultipleSheetsSecond = ExcelFile<"MultipleSheets.xlsx", "B">
+let file = new MultipleSheetsSecond()
+let rows = file.Data |> Seq.toArray
+let test = rows.[1]
+(** And the variable `test` has the following value: *)
+(*** include-value: test ***)
