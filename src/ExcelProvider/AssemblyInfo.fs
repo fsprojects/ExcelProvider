763115e80467629d11381d381ee87a9b0175e0ee
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2016-06-14 17:37:49 +0200

    Bump version to 0.8.0

--- src/ExcelProvider/AssemblyInfo.fs
+++ src/ExcelProvider/AssemblyInfo.fs
@@ -4,9 +4,10 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("ExcelProvider")>]
 [<assembly: AssemblyProductAttribute("ExcelProvider")>]
 [<assembly: AssemblyDescriptionAttribute("This library is for the .NET platform implementing a Excel type provider.")>]
-[<assembly: AssemblyVersionAttribute("0.7.0")>]
-[<assembly: AssemblyFileVersionAttribute("0.7.0")>]
+[<assembly: AssemblyVersionAttribute("0.8.0")>]
+[<assembly: AssemblyFileVersionAttribute("0.8.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.7.0"
+    let [<Literal>] Version = "0.8.0"
+    let [<Literal>] InformationalVersion = "0.8.0"
